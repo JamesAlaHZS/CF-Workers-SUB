c38d0fda87bf67da34c9175830d25360b2adfc44
JamesAlaHZS <163001183+JamesAlaHZS@users.noreply.github.com>
2025-06-09 11:50:51 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -15,9 +15,9 @@ let timestamp = 4102329600000;//2099-12-31
 let MainData = `
 https://raw.githubusercontent.com/mfuu/v2ray/master/v2ray
 `;
-
+//SUBAPI.cmliussss.net
 let urls = [];
-let subConverter = "SUBAPI.cmliussss.net"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+let subConverter = "subapi.ala168.cn"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subConfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
 let subProtocol = 'https';
 
